e2f8357c54b0bca008c5c7318fe1d3681bbce7e1
gkgange <gkgange@gmail.com>
2020-03-20 17:09:25 +1100

    Suppressing warnings in murmurhash.

--- lib/utils/MurmurHash3.cc
+++ lib/utils/MurmurHash3.cc
@@ -52,12 +52,12 @@ inline uint64_t rotl64 ( uint64_t x, int8_t r )
 // Block read - if your platform needs to do endian-swapping or can only
 // handle aligned reads, do the conversion here
 
-static FORCE_INLINE uint32_t getblock ( const uint32_t * p, int i )
+static inline FORCE_INLINE uint32_t getblock ( const uint32_t * p, int i )
 {
   return p[i];
 }
 
-static FORCE_INLINE uint64_t getblock ( const uint64_t * p, int i )
+static inline FORCE_INLINE uint64_t getblock ( const uint64_t * p, int i )
 {
   return p[i];
 }
